7e9baa0e511be736a33f0215a6819d9ab3caccd1
Mohit pal <mohitpal9513@gmail.com>
2024-02-27 21:16:28 +0530

    make nextjs project of music

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import  FearturedCourses  from "../components/FearturedCourses";
+import HeroSection from "../components/HeroSection";
+import MusicSchoolTestimonials from "../components/TestimonialCards";
+import UpcomingWebinars from "../components/UpcomingWebinars";
+import  WhyChooseUs  from "../components/WhyChooseUs";
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="texrt-2xl text-center">chai aur code</h1>
+      <HeroSection/>
+      <FearturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+    </main>
+  );
+}
